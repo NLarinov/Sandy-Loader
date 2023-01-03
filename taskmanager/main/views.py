03b72567ed64cf4b993007bd21f3a823a8d73609
NLarinov <tkachevkolya2006@gmail.com>
2023-01-03 18:55:19 +0300

    First commit

--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .forms import NameForm
-from .main_copy import TestClass
+from .main import TestClass
 import os
 
 
@@ -17,17 +17,17 @@ def index(request):
 
     context = {'form': form, 'firstname': your_link}
     print(your_link)
-
-    if start is False:
-        a = TestClass()
-
-        def start(login):
-            a.easy_download(login)
-            if len(os.listdir('C:\Downloads')) == 0:
-                a.full_download(login)
-            print('ok')
-
-        start('https://pdf.11klasov.net/1825-starlight-5-zvezdnyy-angliyskiy-5-klass-baranova-km-duli-d-kopylova-vv-i-dr.html')
+    #
+    # if start is False:
+    #     a = TestClass()
+    #
+    #     def start(login):
+    #         a.easy_download(login)
+    #         if len(os.listdir('C:\Downloads')) == 0:
+    #             a.full_download(login)
+    #         print('ok')
+    #
+    #     start('https://pdf.11klasov.net/1825-starlight-5-zvezdnyy-angliyskiy-5-klass-baranova-km-duli-d-kopylova-vv-i-dr.html')
 
     start = False
     return render(request, 'main/index.html', context)
